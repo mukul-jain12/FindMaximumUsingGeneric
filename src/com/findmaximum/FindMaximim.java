502c5472e725cdcd7c9e3fa0079bf40810c1411a
Mukul Jain <mukuljain422@gmail.com>
2021-10-21 15:16:53 +0530

    maximum among 3 values

--- src/com/findmaximum/FindMaximim.java
+++ src/com/findmaximum/FindMaximim.java
@@ -3,5 +3,25 @@
 package com.findmaximum;
 
 public class FindMaximim {
+
+	//main method
+	public static void main(String args[]) {
+		
+		//integer values
+		Integer intVal1 = 12, intVal2 = 13, intVal3 = 9;
+		
+		//assign 1st value to max
+		Integer max = intVal1;
+		
+		//checking max with other values
+		if (intVal1.compareTo(max) > 0) {
+			max = intVal2;
+		}
+		if (intVal3.compareTo(max) > 0) {
+			max = intVal3;
+		}
 	
+		//printing maximum value among three
+		System.out.println("Amongst the three the maximum is " + max);
+	}
 }

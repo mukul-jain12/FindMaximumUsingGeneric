8723ee1fa6232e896ebd228f65b0583a96df64a2
Mukul Jain <mukuljain422@gmail.com>
2021-10-21 15:26:47 +0530

    maximum among 3 float

--- src/com/findmaximum/FindMaximim.java
+++ src/com/findmaximum/FindMaximim.java
@@ -8,17 +8,17 @@ public class FindMaximim {
 	public static void main(String args[]) {
 		
 		//Float values
-		Float intVal1 = 12.21f, intVal2 = 18.22f, intVal3 = 9.2f;
+		Float floatVal1 = 12.21f, floatVal2 = 18.22f, floatVal3 = 9.2f;
 		
 		//assign 1st value to max
-		Float max = intVal1;
+		Float max = floatVal1;
 		
 		//checking max with other values
-		if (intVal2.compareTo(max) > 0) {
-			max = intVal2;
+		if (floatVal2.compareTo(max) > 0) {
+			max = floatVal2;
 		}
-		if (intVal3.compareTo(max) > 0) {
-			max = intVal3;
+		if (floatVal3.compareTo(max) > 0) {
+			max = floatVal3;
 		}
 	
 		//printing maximum value among three

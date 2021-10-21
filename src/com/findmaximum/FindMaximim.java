f657dc6ef9e2ff0141a304335a615e3f82034656
Mukul Jain <mukuljain422@gmail.com>
2021-10-21 15:39:10 +0530

    max value using generic

--- src/com/findmaximum/FindMaximim.java
+++ src/com/findmaximum/FindMaximim.java
@@ -6,22 +6,29 @@ public class FindMaximim {
 
 	//main method
 	public static void main(String args[]) {
+		Integer intVal1 = 12, intVal2 = 13, intVal3 = 9;
+		Float floatVal1 = 12.55f, floatVal2 = 13.65f, floatVal3 = 19.22f;
+		String strVal1 = "Ram", strVal2 = "Chahal", strVal3 = "Pahal";
 		
-		//Float values
-		String strVal1 = "Ram", strVal2 = "Shaam", strVal3 = "Sagar";
+		//call generic method
+		toprint(intVal1, intVal2, intVal3);
+		toprint(floatVal1, floatVal2, floatVal3);
+		toprint(strVal1, strVal2, strVal3);
+	}
+
+	//generic method
+	private static <E extends Comparable<E>> void toprint(E val1, E val2, E val3) {
 		
-		//assign 1st value to max
-		String max = strVal1;
+		//assign value to max
+		E max = val1;
 		
-		//checking max with other values
-		if (strVal2.compareTo(max) > 0) {
-			max = strVal2;
+		//check value is greater of not
+		if (val2.compareTo(max) > 0) {
+			max = val2;
 		}
-		if (strVal3.compareTo(max) > 0) {
-			max = strVal3;
+		if (val3.compareTo(max) > 0) {
+			max = val3;
 		}
-	
-		//printing maximum value among three
 		System.out.println("Amongst the three the maximum is " + max);
 	}
 }
